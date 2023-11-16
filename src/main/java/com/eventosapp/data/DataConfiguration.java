5405ea93167d76a991638a7b02f766f84382dd4b
igorAlves321 <igoralvespalmeirag4@gmail.com>
2023-11-15 22:53:31 -0300

    configurado o Hibernate e corrigido alguns bugs como chaves duplicadas ao criar as tabelas e erro no arquivo de configuração do spring.

--- src/main/java/com/eventosapp/data/DataConfiguration.java
+++ src/main/java/com/eventosapp/data/DataConfiguration.java
@@ -1,8 +1,6 @@
 package com.eventosapp.data;
 
-
 import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,22 +14,22 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 public class DataConfiguration {
 
 	@Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        return dataSource;
-    }
-	
+	public DataSource dataSource(){
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); // Atualizado para o novo driver
+		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useTimezone=true&serverTimezone=UTC"); // Adicionado parâmetros para timezone
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
 	@Bean
 	public JpaVendorAdapter jpaVendorAdapter(){
 		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
 		adapter.setDatabase(Database.MYSQL);
 		adapter.setShowSql(true);
 		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect"); // Alterado para o dialeto específico do MySQL 5, se necessário
 		adapter.setPrepareConnection(true);
 		return adapter;
 	}
